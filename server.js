2491ee65a0a148bd4f3b1b842fd753dbb5cac911
Elias Ferede <feredeelias@gmail.com>
2020-10-15 18:51:45 -0700

    Added port number

--- server.js
+++ server.js
@@ -1,17 +1,7 @@
-// server.js
-// where your node app starts
-
 // init project
 const express = require('express');
 const isValid = require('date-fns/isValid');
 const app = express();
-
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-const cors = require('cors');
-app.use(cors({
-  optionsSuccessStatus: 200
-})); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -55,8 +45,10 @@ app.get("/api/timestamp/:date_string", function (req, res) {
   }
 });
 
+// Setting Port
+const PORT = process.env.PORT || 3000;
 
 // listen for requests :)
-const listener = app.listen(process.env.PORT, function () {
+const listener = app.listen(PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
